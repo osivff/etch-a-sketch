03c8e00db4e26d7847b9e601ef6d7b6d4b4d7b91
osivff <oscar.fitchfierro@gmail.com>
2025-04-15 09:48:25 -0700

    User dynamically updates board size

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
 const section = document.querySelector('section');
 const erase = document.querySelector('.erase');
+const sizeBtn = document.querySelector('.btn-size');
+let userInput = 16;
 
-for(i = 0; i <= 15; i++){
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    section.appendChild(row);
-    for(j = 0; j <= 15; j++){
-        const col = document.createElement('div');
-        col.setAttribute('class', 'col');
-        row.appendChild(col);
+function createBoard(size) {
+    for(i = 0; i < size; i++){
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        section.appendChild(row);
+        for(j = 0; j < size; j++){
+            const col = document.createElement('div');
+            col.setAttribute('class', 'col');
+            row.appendChild(col);
+        }
     }
 }
 
-const col = document.querySelectorAll('.col');
+createBoard(userInput);
+
+function updateBoard(){
+    const row = document.querySelectorAll('.row');
+    for(i = 0; i <= row.length - 1; i++){
+        section.removeChild(row[i]);
+    }
+    
+}
+
+sizeBtn.addEventListener('click', () => {
+    userInput = parseInt(prompt('Choose a size for your board'));
+    updateBoard();
+    createBoard(userInput);
+});
+
 
 section.addEventListener('mouseover', (e) => {
     if(e.target.className === 'col') {
@@ -21,7 +40,9 @@ section.addEventListener('mouseover', (e) => {
 });
 
 erase.addEventListener('click', () => {
+    let col = document.querySelectorAll('.col');
     for(i = 0; i <= col.length -1; i++){
         col[i].style.backgroundColor = 'white';
     }
+    console.log(col);
 });
