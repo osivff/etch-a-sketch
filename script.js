8b2c48bfef340e58f52053c485cf86f6d688c367
osivff <oscar.fitchfierro@gmail.com>
2025-04-15 14:35:02 -0700

    Add multi-color background and change opacity levels

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function createNewBoard(){
     if(isNaN(userInput)) {
         userInput = prompt('Please type a number btween 1 and 50: ');
     } else if (userInput > 50){
-        userInput = prompt('Please enter a size between 1 and 50: ');
+        userInput = prompt('The number is too big. Enter a num between 1 adn 50: ');
     } else if(userInput <= 0) {
         userInput = prompt('Please enter a larger size: ');
     }
@@ -41,16 +41,33 @@ function createNewBoard(){
 function eraseCol(){
     let col = document.querySelectorAll('.col');
     for(i = 0; i <= col.length -1; i++){
-        col[i].style.backgroundColor = 'white';
+        col[i].style.backgroundColor = 'rgba(255, 255, 255, 1)';
     }
+};
+
+function randomColor(){
+    let randomNum = Number(Math.floor((Math.random() * 255)) + 1);
+    return randomNum;
 };
 
 sizeBtn.addEventListener('click', createNewBoard);
 
+let initialNum = 0.1
+let currentOpacity = parseFloat(initialNum.toFixed(1)); 
+
 section.addEventListener('mouseover', (e) => {
     if(e.target.className === 'col') {
-        e.target.style.backgroundColor = "black";
-    }
+        let red = randomColor();
+        let green = randomColor();
+        let blue = randomColor(); 
+        let addNumber = 0.1;
+        if(currentOpacity >= 0.1 && currentOpacity <= 0.9){
+            e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${currentOpacity})`;
+            currentOpacity = currentOpacity + parseFloat(addNumber.toFixed(1));
+        } else{
+            currentOpacity = parseFloat(initialNum.toFixed(1));
+        };
+    };
 });
 
 erase.addEventListener('click', eraseCol);
